1144afb7a8d144f6417a8cef660f5d02693a55b1
Official-Krish <krishanand0103@gmail.com>
2025-01-28 23:57:44 +0530

    half ass fe

---
+++ frontend/src/components/Appbar.tsx
@@ -0,0 +1,34 @@
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+
+export function Appbar() {
+  const { address } = useAccount()
+
+  return <div className='flex justify-between p-2 m-2'>
+    <div className='text-2xl'>
+        Stakify
+    </div>
+    <div>
+        {!address ? <Connectors /> : <Disconnect />}
+    </div>
+  </div>
+}
+
+function Connectors() {
+    const { connectors, connect } = useConnect()
+    return connectors.map((connector) => (
+        <button className='mx-2 border rounded p-2' key={connector.uid} onClick={() => connect({ connector })}>
+        {connector.name}
+        </button>
+    ))
+}
+
+function Disconnect() {
+    const {disconnect} = useDisconnect();
+    
+    return <div>
+        <button className='mx-2 border rounded p-2' onClick={() => disconnect()}>
+            Disconnect wallet
+        </button>
+    </div>
+
+}
